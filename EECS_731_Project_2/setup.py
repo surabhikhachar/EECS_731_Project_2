e731687b2832ed4c64e2f70d6e7bab2ff6273212
Surabhi Khachar <surabhikhachar@ku.edu>
2018-10-02 22:03:29 -0500

    Updating with file structure for data science cookie cutter project

---
+++ EECS_731_Project_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To be or not to be',
+    author='Surabhi Khachar',
+    license='',
+)
